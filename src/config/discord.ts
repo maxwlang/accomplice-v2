71749a3ffdb7436a1df35a7a4f2c74c3a49145bc
Maxwell Lang <maxwlang@gmail.com>
2023-05-23 02:38:38 -0400

    Some configs

--- src/config/discord.ts
+++ src/config/discord.ts
@@ -2,5 +2,11 @@ export const token =
     process.env['DISCORD_TOKEN'] ||
     'MTA0NTE1MDc2OTM4MjQyNDU4Ng.G-b-f5.FYvO7sRjsBGxDE1ZJqsKcFQ7CKiR0-NtWaNYuY'
 
-export const avatarUrl = ''
-export const avatarDisplayName = 'Accomplice (Beta)'
+export const avatarUrl = process.env['DISCORD_AVATAR_URL']
+export const avatarDisplayName =
+    process.env['DISCORD_DISPLAY_NAME'] || 'Accomplice V2'
+
+export const activityRefreshInterval =
+    (process.env['DISCORD_ACTIVITY_REFRESH_INTERVAL']
+        ? +process.env['DISCORD_ACTIVITY_REFRESH_INTERVAL']
+        : 60) * 1000
