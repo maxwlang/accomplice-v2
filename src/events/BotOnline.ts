2abd2030a21bd8362c722213ce1a1d9d9ac58198
Maxwell Lang <maxwlang@gmail.com>
2023-05-23 02:39:01 -0400

    Bot online handler

--- src/events/BotOnline.ts
+++ src/events/BotOnline.ts
@@ -1,14 +1,19 @@
 import Accomplice from '../accomplice'
 import EventHandle from '../types/EventHandle'
+import { avatarDisplayName, avatarUrl } from '../config/discord'
 
 export default class BotOnline implements EventHandle {
     public name = 'Bot Online'
-    public description = 'Shows a simple message when logged in'
+    public description = 'Handles small tasks once logged in'
     public trigger = 'ready'
     public fireOnce = true
 
     public execute = async ({ bot }: { bot: Accomplice }): Promise<void> => {
         bot.logger.info('Accomplice has logged in!')
+        if (bot.user) {
+            if (avatarDisplayName) bot.user.setUsername(avatarDisplayName)
+            if (avatarUrl) bot.user.setAvatar(avatarUrl)
+        }
         return
     }
 }
