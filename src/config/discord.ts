d2fe1db7a6749b79149e9f7df441c57cefe1c28d
Maxwell Lang <maxwlang@gmail.com>
2023-05-26 01:16:41 -0500

    DISCORD_BOT_ADMIN_SNOWFLAKE

--- src/config/discord.ts
+++ src/config/discord.ts
@@ -10,3 +10,6 @@ export const activityRefreshInterval =
     (process.env['DISCORD_ACTIVITY_REFRESH_INTERVAL']
         ? +process.env['DISCORD_ACTIVITY_REFRESH_INTERVAL']
         : 60) * 1000
+
+export const botAdminUserSnowflake =
+    process.env['DISCORD_BOT_ADMIN_SNOWFLAKE'] ?? '707022657354203180'
