8a67eb700bff1b3c7b8d33016e106e5c2cd7a014
Maxwell Lang <maxwlang@gmail.com>
2023-06-01 07:10:47 -0500

    Rename

--- src/config/discord.ts
+++ src/config/discord.ts
@@ -1,10 +1,10 @@
 export const token =
     process.env['DISCORD_TOKEN'] ||
-    // 'MTA0NTE1MDc2OTM4MjQyNDU4Ng.G-b-f5.FYvO7sRjsBGxDE1ZJqsKcFQ7CKiR0-NtWaNYuY'
-    'NzMzOTI5MDU0OTMxMjU1MzI3.GNfX7W.solZTu9NpyPiDrNcv031EHkGqqsGaAfqwRsQUM'
+    'MTA0NTE1MDc2OTM4MjQyNDU4Ng.G-b-f5.FYvO7sRjsBGxDE1ZJqsKcFQ7CKiR0-NtWaNYuY'
+// 'NzMzOTI5MDU0OTMxMjU1MzI3.GNfX7W.solZTu9NpyPiDrNcv031EHkGqqsGaAfqwRsQUM'
 
 export const avatarDisplayName =
-    process.env['DISCORD_DISPLAY_NAME'] || 'Accomplice V2'
+    process.env['DISCORD_DISPLAY_NAME'] || 'Accomplice'
 
 export const activityRefreshInterval =
     (process.env['DISCORD_ACTIVITY_REFRESH_INTERVAL']
